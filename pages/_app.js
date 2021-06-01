2ea4ab133468c3bc145cea94956e3957a7dfc42f
its-Saab <musaababs@gmail.com>
2021-06-01 21:35:46 +0200

    Import styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
-import '../styles/globals.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'highlight.js/styles/arta.css'
+import 'styles/index.scss'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
