cd8d5aec3792d3993b6d9b5c5ee47ceda36b1710
its-Saab <musaababs@gmail.com>
2021-06-02 19:03:24 +0200

    Import date icons from fontawesome

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,13 @@
 //npm packages
 import { library, config } from "@fortawesome/fontawesome-svg-core";
-import { faList, faBorderAll } from "@fortawesome/free-solid-svg-icons";
+import {
+  faSun,
+  faMoon,
+  faBorderAll,
+  faList,
+  faSortNumericDown,
+  faSortNumericUp } from '@fortawesome/free-solid-svg-icons';
+
 //styles
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -8,7 +15,13 @@ import "highlight.js/styles/arta.css";
 import "styles/index.scss";
 
 config.autoAddCss = false;
-library.add(faList, faBorderAll);
+library.add(
+  faSun,
+  faMoon,
+  faList,
+  faBorderAll,
+  faSortNumericDown,
+  faSortNumericUp);
 
 function MyApp({ Component, pageProps }) {
 	return <Component {...pageProps} />;
