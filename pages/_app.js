ea73bd558d019494697032d60d16c3b799b23831
its-Saab <musaababs@gmail.com>
2021-06-02 11:53:17 +0200

    Add fontawesome imports

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,17 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'highlight.js/styles/arta.css'
-import 'styles/index.scss'
+//npm packages
+import { library, config } from "@fortawesome/fontawesome-svg-core";
+import { faList, faBorderAll } from "@fortawesome/free-solid-svg-icons";
+//styles
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "highlight.js/styles/arta.css";
+import "styles/index.scss";
+
+config.autoAddCss = false;
+library.add(faList, faBorderAll);
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
