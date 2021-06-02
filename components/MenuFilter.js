01365727d368d5716360a0e51f742866aa746181
its-Saab <musaababs@gmail.com>
2021-06-02 19:02:10 +0200

    Add filter by view / date

--- components/MenuFilter.js
+++ components/MenuFilter.js
@@ -2,17 +2,24 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 //project files
 
-export const MenuFilter = ({ view,onChange }) => {
-	const handleClick = (e) => {
-		return onChange(e);
-	};
+const LIST_VIEW_ICONS = ["list", "border-all"];
+const DATE_FILTERING_ICONS = ["sort-numeric-down", "sort-numeric-up"];
+
+export const MenuFilter = ({ onChange, filter }) => {
+
 	return (
 		<div className="filtering-menu mb-2">
 			<FontAwesomeIcon
 				size="lg"
-				icon={view? "border-all":"list"}
-				className="clickable hoverable"
-				onClick={handleClick}
+				icon={LIST_VIEW_ICONS[filter.view.list]}
+				className="clickable hoverable mr-3"
+				onClick={()=> onChange('view', {list: +!filter.view.list})}
+			/>
+			<FontAwesomeIcon
+				className="clickable hoverable mr-3"
+				size="lg"
+				icon={DATE_FILTERING_ICONS[filter.date.asc]}
+				onClick={() => onChange('date', {asc: +!filter.date.asc})}
 			/>
 		</div>
 	);
